a6cebbc9de590510ed65d093f91d94465acb4221
Essa Khan <essa.khan.pro@gmail.com>
2021-03-31 04:01:11 +0100

    fixes

--- ssh.py
+++ ssh.py
@@ -1,13 +1,5 @@
 import spur
 
-def launchFileThroughSSHlowestPopularRating():
-    shell = spur.SshShell(hostname="127.0.0.1", port=2222, username="maria_dev", password="maria_dev",
-                          missing_host_key=spur.ssh.MissingHostKey.accept)
-
-    with shell:
-        result = shell.run(["spark-submit", "LowestRatedPopularMovie.py"]) # LowestRatedPopularMovieDataFrame.py
-    return result.output.decode('utf-8')
-
 def launchFileThroughSSHlowestRating():
     shell = spur.SshShell(hostname="127.0.0.1", port=2222, username="maria_dev", password="maria_dev",
                           missing_host_key=spur.ssh.MissingHostKey.accept)
@@ -16,11 +8,21 @@ def launchFileThroughSSHlowestRating():
         result = shell.run(["spark-submit", "LowestRatedMovie.py"]) # LowestRatedPopularMovieDataFrame.py
     return result.output.decode('utf-8')
 
+def launchFileThroughSSHlowestPopularRating():
+    shell = spur.SshShell(hostname="127.0.0.1", port=2222, username="maria_dev", password="maria_dev",
+                          missing_host_key=spur.ssh.MissingHostKey.accept)
+
+    with shell:
+        result = shell.run(["spark-submit", "LowestRatedPopularMovie.py"]) # LowestRatedPopularMovieDataFrame.py
+    return result.output.decode('utf-8')
+
+
+
 def launchFileThroughSSHpopular():
     shell = spur.SshShell(hostname="127.0.0.1", port=2222, username="maria_dev", password="maria_dev",
                           missing_host_key=spur.ssh.MissingHostKey.accept)
 
     with shell:
-        result = shell.run(["spark-submit", "pop2.py"])
+        result = shell.run(["spark-submit", "PopularMovie.py"])
     return result.output.decode('utf-8')
 
