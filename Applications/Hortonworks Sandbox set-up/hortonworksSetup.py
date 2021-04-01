f1c6078755e5b17333884ec36e132ea5c97825ba
Essa Khan <essa.khan.pro@gmail.com>
2021-04-01 16:04:24 +0100

    code improvements

--- Applications/Hortonworks Sandbox set-up/hortonworksSetup.py
+++ Applications/Hortonworks Sandbox set-up/hortonworksSetup.py
@@ -1,7 +1,9 @@
 import PySimpleGUI as sg
 import spur
 
-def SSHstuff():
+###############################SSH Script###########################################
+
+def SSHscript():
     shell = spur.SshShell(hostname="127.0.0.1", port=2222, username="maria_dev", password="maria_dev",
                           missing_host_key=spur.ssh.MissingHostKey.accept)
 
@@ -15,7 +17,7 @@ def SSHstuff():
         
         #Storing u.item onto local
         shell.run(["curl", "-O", "https://raw.githubusercontent.com/essakh/spark-application/master/Spark%20files/ml-100k/u.item"])
-        #shell.run(["rm", "-r", "ml-100k"])
+        #shell.run(["rm", "-rf", "ml-100k"])
         shell.run(["mkdir", "ml-100k"])
         shell.run(["mv", "u.item" ,"ml-100k"])
         
@@ -24,10 +26,14 @@ def SSHstuff():
         shell.run(["curl", "-O", "https://raw.githubusercontent.com/essakh/spark-application/master/Spark%20files/LowestRatedPopularMovie.py"])
         shell.run(["curl", "-O", "https://raw.githubusercontent.com/essakh/spark-application/master/Spark%20files/PopularMovie.py"])
         
+
+###############################GUI###########################################
+        
 sg.theme('BluePurple')
 
-layout = [[sg.Text('Please press the button below to auto download necessary files and configure your Hortonworks sandbox')],
-          [sg.Text('Once you press the Start button please wait patiently until a pop-up shows confirming process completion (takes less than 2 minutes ussually)')],
+layout = [[sg.Text('This application should be run once before running MainApplication program.\n')],
+          [sg.Text('Please press the button below to auto download necessary files and configure your Hortonworks sandbox')],
+          [sg.Text('Once you press the Start button please wait patiently until a pop-up shows confirming process completion (takes less than 2 minutes ussually) \n')],
           [sg.Button('Start')]]
 
 window = sg.Window('CS3800: Auto Configure Hortonworks sandbox application', layout)
@@ -39,8 +45,9 @@ while True:  # Event Loop
         break
 
     if event == 'Start':
-        SSHstuff()
-        sg.popup("Finished!", title = 'Message')
+        SSHscript()
+        sg.popup("Finished! You can close this application and launch MainApplication", title = 'Success!')
+
 
 
 
--- Applications/Main Application/MainApplication.py
+++ Applications/Main Application/MainApplication.py
@@ -3,13 +3,14 @@ import ssh
 
 sg.theme('BluePurple')
 
-layout = [[sg.Text('Please press one of the following buttons to query the data')],
+layout = [[sg.Text('This program should be used only after you have ran hortonworksSetup application once \n')],
+          [sg.Text('Please press one of the following buttons to query the data')],
           [sg.Button('Show top ten worst rated movies')],
           [sg.Button('Show top ten worst rated movies with >= 10 total ratings')],
           [sg.Button('Show top 10 Most Popular Movies')],
-          [sg.Text('Once you press a button please wait patiently until a pop-up shows displaying the results (takes less than 3 minutes ussually)')]]
+          [sg.Text('\n Once you press a button please wait patiently until a pop-up shows displaying the results (takes less than 3 minutes usually)')]]
 
-window = sg.Window('Essa Khan CS3800 application', layout)
+window = sg.Window('Essa Khan CS3800 Main Application', layout)
 
 while True:  # Event Loop
     event, values = window.read()
@@ -19,11 +20,11 @@ while True:  # Event Loop
 
     if event == 'Show top ten worst rated movies':
         #Call method to Submit respective spark script through SSH across cluster and display results in new pop-up window
-        sg.popup_scrolled("Movie name | Tot. Num. of people that left rating | Avg. rating\n", ssh.launchFileThroughSSHlowestRating(), title = 'Top ten worst rated movies', non_blocking = True, location=(0, 0))
+        sg.popup_scrolled("Movie name | Tot. Num. of rating submissions | Avg. rating\n", ssh.launchFileThroughSSHlowestRating(), title = 'Top ten worst rated movies', non_blocking = True, location=(0, 0))
         
-    if event == 'Show top ten worst rated movies with >= 10 total ratings':
+    if event == 'Show top ten worst rated movies with >= 10 rating submissions':
         #Call method to Submit respective spark script through SSH across cluster and display results in new pop-up window
-        sg.popup_scrolled("Movie name | Tot. Num. of people that left rating | Avg. rating\n", ssh.launchFileThroughSSHlowestPopularRating(), title = 'Top ten worst rated movies with >= 10 total ratings', non_blocking = True, location=(0,0))
+        sg.popup_scrolled("Movie name | Tot. Num. of rating submissions | Avg. rating\n", ssh.launchFileThroughSSHlowestPopularRating(), title = 'Top ten worst rated movies with >= 10 total ratings', non_blocking = True, location=(0,0))
 
     if event == 'Show top 10 Most Popular Movies':
         #Call method to Submit respective spark script through SSH across cluster and display results in new pop-up window
