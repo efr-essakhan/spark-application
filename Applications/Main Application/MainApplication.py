a19a9ccf7e0086842caf60bcc703cd39068a87ff
Essa Khan <essa.khan.pro@gmail.com>
2021-04-02 04:36:35 +0100

    finalisation

--- Applications/Main Application/MainApplication.py
+++ Applications/Main Application/MainApplication.py
@@ -6,7 +6,7 @@ sg.theme('BluePurple')
 layout = [[sg.Text('This program should be used only after you have ran hortonworksSetup application once \n')],
           [sg.Text('Please press one of the following buttons to query the data')],
           [sg.Button('Show top ten worst rated movies')],
-          [sg.Button('Show top ten worst rated movies with >= 10 total ratings')],
+          [sg.Button('Show top ten worst rated movies with >= 10 rating submissions')],
           [sg.Button('Show top 10 Most Popular Movies')],
           [sg.Text('\n Once you press a button please wait patiently until a pop-up shows displaying the results (takes less than 3 minutes usually)')]]
 
@@ -24,10 +24,10 @@ while True:  # Event Loop
         
     if event == 'Show top ten worst rated movies with >= 10 rating submissions':
         #Call method to Submit respective spark script through SSH across cluster and display results in new pop-up window
-        sg.popup_scrolled("Movie name | Tot. Num. of rating submissions | Avg. rating\n", ssh.launchFileThroughSSHlowestPopularRating(), title = 'Top ten worst rated movies with >= 10 total ratings', non_blocking = True, location=(0,0))
+        sg.popup_scrolled("Movie name | Tot. Num. of rating submissions | Avg. rating\n", ssh.launchFileThroughSSHlowestPopularRating(), title = 'Top ten worst rated movies with >= 10 rating submissions', non_blocking = True, location=(0,0))
 
     if event == 'Show top 10 Most Popular Movies':
         #Call method to Submit respective spark script through SSH across cluster and display results in new pop-up window
-        sg.popup_scrolled("Movie name | Total number of ratings\n", ssh.launchFileThroughSSHpopular(),
+        sg.popup_scrolled("Movie name | Tot. Num. of rating submissions\n", ssh.launchFileThroughSSHpopular(),
                  title='Top 10 Most Popular Movies', non_blocking=True, location=(0,0))
 window.close()
